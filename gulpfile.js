43cc3156271efa49e67af3d9ae8fcb16104a5d25
ddz2456820347 <2456820347@qq.com>
2023-07-30 00:05:51 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ const uglify = require("gulp-uglify");
 
 const SOURCES = {
   scss: "styles/**/*.scss",
-  css: "styles/**/*.css",
   scripts: "script/**/*.js",
 };
 
@@ -20,12 +19,9 @@ const compileSass = () =>
     .pipe(cleanCSS())
     .pipe(gulp.dest(DESTINATIONS.assets));
 
-const watch = () => gulp.watch(SOURCES.styles, gulp.series(compileSass));
-
-const minifyCSS = () =>
-  gulp.src(SOURCES.css).pipe(cleanCSS()).pipe(gulp.dest(DESTINATIONS.assets));
+const watch = () => gulp.watch(SOURCES.scss, gulp.series(compileSass));
 
 const minifyJS = () =>
   gulp.src(SOURCES.scripts).pipe(uglify()).pipe(gulp.dest(DESTINATIONS.assets));
 
-exports.default = gulp.series(gulp.parallel(minifyCSS, minifyJS));
+exports.default = gulp.series(watch, gulp.parallel(compileSass, minifyJS));
