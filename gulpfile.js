4f94bd7bb23c5d37467262bed7f884f67b239a03
ddz2456820347 <2456820347@qq.com>
2023-07-30 13:30:00 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,6 @@ const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const cleanCSS = require("gulp-clean-css");
 const uglify = require("gulp-uglify");
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
 
 const SOURCES = {
   scss: "styles/**/*.scss",
@@ -26,12 +24,4 @@ const watch = () => gulp.watch(SOURCES.scss, gulp.series(compileSass));
 const minifyJS = () =>
   gulp.src(SOURCES.scripts).pipe(uglify()).pipe(gulp.dest(DESTINATIONS.assets));
 
-function postCss() {
-  return new Promise((res, rej) => {
-    res(gulp.src(SOURCES.scss));
-  }).then((value) => {
-    value.pipe(postcss([autoprefixer()])).pipe(gulp.dest(DESTINATIONS.assets));
-  });
-}
-
-exports.default = gulp.series(gulp.parallel(compileSass, minifyJS, postCss));
+exports.default = gulp.series(watch, gulp.parallel(compileSass, minifyJS));
