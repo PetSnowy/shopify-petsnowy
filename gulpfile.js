d6198dff4d64bec45522e9a5525796b5822d3322
ddz2456820347 <2456820347@qq.com>
2023-08-01 11:00:30 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const cleanCSS = require("gulp-clean-css");
 const uglify = require("gulp-uglify");
-const clean = require("gulp-clean");
 
 const SOURCES = {
   scss: "styles/**/*.scss",
@@ -22,16 +21,10 @@ const compileSass = () =>
 
 const watch = () => gulp.watch(SOURCES.scss, gulp.series(compileSass));
 
-const deleteFiles = () =>
-  gulp
-    .src(DESTINATIONS.assets, { read: false, allowEmpty: true })
-    .pipe(clean());
-
 const minifyJS = () =>
   gulp.src(SOURCES.scripts).pipe(uglify()).pipe(gulp.dest(DESTINATIONS.assets));
 
 exports.default = gulp.series(
-  deleteFiles,
   gulp.parallel(compileSass, minifyJS),
   watch
 );
