5c39aca86b31cb10a06d12212b2b6e3617cc581c
ddz2456820347 <2456820347@qq.com>
2023-08-09 11:58:41 +0800

    fix faq bug

--- gulpfile.js
+++ gulpfile.js
@@ -4,27 +4,31 @@ const cleanCSS = require("gulp-clean-css");
 const uglify = require("gulp-uglify");
 
 const SOURCES = {
-  scss: "styles/**/*.scss",
-  scripts: "script/**/*.js",
+	scss: "styles/**/*.scss",
+	scripts: "script/**/*.js",
 };
 
 const DESTINATIONS = {
-  assets: "assets",
+	assets: "assets",
 };
 
 const compileSass = () =>
-  gulp
-    .src(SOURCES.scss)
-    .pipe(sass().on("error", sass.logError))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest(DESTINATIONS.assets));
-
-const watch = () => gulp.watch(SOURCES.scss, gulp.series(compileSass));
+	gulp
+		.src(SOURCES.scss)
+		.pipe(sass().on("error", sass.logError))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest(DESTINATIONS.assets));
 
 const minifyJS = () =>
-  gulp.src(SOURCES.scripts).pipe(uglify()).pipe(gulp.dest(DESTINATIONS.assets));
+	gulp.src(SOURCES.scripts).pipe(uglify()).pipe(gulp.dest(DESTINATIONS.assets));
+
+const watch = () => {
+	gulp.watch(SOURCES.scripts, gulp.series(minifyJS));
+	gulp.watch(SOURCES.scss, gulp.series(compileSass))
+}
+
 
 exports.default = gulp.series(
-  gulp.parallel(compileSass, minifyJS),
-  watch
+	gulp.parallel(compileSass, minifyJS),
+	watch
 );
