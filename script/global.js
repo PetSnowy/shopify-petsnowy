ca2ece5578478e523a25fa3287e051fc09042be9
ddz2456820347 <2456820347@qq.com>
2023-09-26 15:17:23 +0800

    add product-recommendation

--- script/global.js
+++ script/global.js
@@ -828,7 +828,7 @@ class VariantSelects extends HTMLElement {
 		const product = document.querySelector('product-form');
 		product && product.updateOption()
 
-		if (this.currentVariant && this.currentVariant.available && pickUpAvailability.fetchAvailability) {
+		if (this.currentVariant && this.currentVariant.available) {
 			pickUpAvailability.fetchAvailability(this.currentVariant.id);
 		} else {
 			pickUpAvailability.removeAttribute('available');
@@ -911,42 +911,42 @@ class VariantRadios extends VariantSelects {
 
 customElements.define('variant-radios', VariantRadios);
 
-class ProductRecommendations extends HTMLElement {
-	constructor() {
-		super();
-	}
+// class ProductRecommendations extends HTMLElement {
+// 	constructor() {
+// 		super();
+// 	}
 
-	connectedCallback() {
-		const handleIntersection = (entries, observer) => {
-			if (!entries[0].isIntersecting) return;
-			observer.unobserve(this);
+// 	connectedCallback() {
+// 		const handleIntersection = (entries, observer) => {
+// 			if (!entries[0].isIntersecting) return;
+// 			observer.unobserve(this);
 
-			fetch(this.dataset.url)
-				.then(response => response.text())
-				.then(text => {
-					const html = document.createElement('div');
-					html.innerHTML = text;
-					const recommendations = html.querySelector('product-recommendations');
+// 			fetch(this.dataset.url)
+// 				.then(response => response.text())
+// 				.then(text => {
+// 					const html = document.createElement('div');
+// 					html.innerHTML = text;
+// 					const recommendations = html.querySelector('product-recommendations');
 
-					if (recommendations && recommendations.innerHTML.trim().length) {
-						this.innerHTML = recommendations.innerHTML;
-					}
+// 					if (recommendations && recommendations.innerHTML.trim().length) {
+// 						this.innerHTML = recommendations.innerHTML;
+// 					}
 
-					if (!this.querySelector('slideshow-component') && this.classList.contains('complementary-products')) {
-						this.remove();
-					}
+// 					if (!this.querySelector('slideshow-component') && this.classList.contains('complementary-products')) {
+// 						this.remove();
+// 					}
 
-					if (html.querySelector('.grid__item')) {
-						this.classList.add('product-recommendations--loaded');
-					}
-				})
-				.catch(e => {
-					console.error(e);
-				});
-		}
+// 					if (html.querySelector('.grid__item')) {
+// 						this.classList.add('product-recommendations--loaded');
+// 					}
+// 				})
+// 				.catch(e => {
+// 					console.error(e);
+// 				});
+// 		}
 
-		new IntersectionObserver(handleIntersection.bind(this), { rootMargin: '0px 0px 400px 0px' }).observe(this);
-	}
-}
+// 		new IntersectionObserver(handleIntersection.bind(this), { rootMargin: '0px 0px 400px 0px' }).observe(this);
+// 	}
+// }
 
-customElements.define('product-recommendations', ProductRecommendations);
+// customElements.define('product-recommendations', ProductRecommendations);
--- script/product-recommendation.js
+++ script/product-recommendation.js
@@ -1,7 +1,21 @@
 class ProductRecommendation extends HTMLElement {
 	constructor() {
-		super()
-		this.productId = this.getAttribute('product-id')
+		super();
+		this.productId = this.getAttribute('product-id');
+		this.fetchProductRecommendation(this.productId, 1, 'related').then((productRecommendationList) => {
+			console.log(productRecommendationList);
+		})
+	}
+
+	async fetchProductRecommendation(productId, limit, intent) {
+		try {
+			const response = await fetch(window.Shopify.routes.root + `recommendations/products.json?product_id=${productId}&limit=${limit}&intent=${intent}`);
+			const { products } = await response.json();
+			return products.length && products;
+		} catch (error) {
+			console.log(error);
+		}
 	}
 }
-customElements.define('product-recommendation', ProductRecommendation)
+
+customElements.define('product-recommendation', ProductRecommendation);
