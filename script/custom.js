dcbfeeeb09f04cd6a6243447289cb6ef4dfefe9a
ddz2456820347 <2456820347@qq.com>
2023-11-14 16:53:58 +0800

    update product

--- script/custom.js
+++ script/custom.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
 
 	$(".slide_arrow svg").click(function () {
 		const el = document.querySelector('.active-img') || document.querySelector('#product-indiegogo')
-		document.documentElement.style.scrollPaddingTop = paddingTop;
+		document.querySelector('#product-indiegogo') && document.documentElement.style.scrollPaddingTop = paddingTop;
 		el.scrollIntoView({ behavior: "smooth", block: "start" })
 	});
 
